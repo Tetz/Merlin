145111bf456de7b9e49fa0cfd07c3170a924a0da
Takemoto Tetsuro <t@ineeza.com>
2021-06-10 17:32:30 +0900

    Initial commit :rocket:

---
+++ Merlin/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Merlin
+//
+//  Created by t on 2021/06/10.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Merlin!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Merlin/MerlinApp.swift
@@ -0,0 +1,18 @@
+//
+//  MerlinApp.swift
+//  Merlin
+//
+//  Created by t on 2021/06/10.
+//
+//
+
+import SwiftUI
+
+@main
+struct MerlinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
